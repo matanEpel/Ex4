9892e826b2aee11671f7b8aa685336224068a917
matanEpel <59418285+matanEpel@users.noreply.github.com>
2020-01-20 16:44:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 
-#include "interfaces.h"
-#include "StringReverser.h"
-#include "MyTestClientHandler.h"
-#include "MySerialServer.h"
-#include "FileCacheManager.h"
-#include "server_side.h"
+#include "searchAlgorithms/MatrixGraphProblem.h"
+#include "server/server_side.h"
 
 using namespace boot;
 
-int main(int argc, char const *argv[]) {
-  int port = atoi(argv[1]);
-
+int main(int argc, char const *argv[])
+{
+  int port = 5400;//atoi(argv[1]);
   Main mainRun;
   mainRun.main(port);
+
+
   return 0;
 }
